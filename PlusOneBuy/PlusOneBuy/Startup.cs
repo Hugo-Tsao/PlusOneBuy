7452912e9454c2025936e3ceb2e6e85e02dc57d1
Hugo-Tsao <hugotsao@nine-yi.com>
2019-07-03 12:06:57 +0800

    第一次建立環境專案

---
+++ PlusOneBuy/PlusOneBuy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlusOneBuy.Startup))]
+namespace PlusOneBuy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

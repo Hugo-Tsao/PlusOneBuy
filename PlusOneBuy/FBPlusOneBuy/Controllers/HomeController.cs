6e90ba2f262b2944db94354a265c205f600444e6
JimChou1101 <jimchou@nine-yi.com>
2019-07-03 18:49:52 +0800

    隱私政策again

--- PlusOneBuy/FBPlusOneBuy/Controllers/HomeController.cs
+++ PlusOneBuy/FBPlusOneBuy/Controllers/HomeController.cs
@@ -12,6 +12,10 @@ namespace FBPlusOneBuy.Controllers
         {
             return View();
         }
+        public ActionResult Privacy()
+        {
+            return View();
+        }
 
         public ActionResult About()
         {

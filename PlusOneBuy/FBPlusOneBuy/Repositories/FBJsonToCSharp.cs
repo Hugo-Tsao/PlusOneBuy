c3f13547556b5a614f6d8c05380163d9a1191906
Hugo-Tsao <hugotsao@nine-yi.com>
2019-07-04 19:14:23 +0800

    新增FBJson格式

---
+++ PlusOneBuy/FBPlusOneBuy/Repositories/FBJsonToCSharp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FBPlusOneBuy.Repositories
+{
+    public class From
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Datum
+    {
+        public DateTime created_time { get; set; }
+        public From from { get; set; }
+        public string story { get; set; }
+        public string message { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Comments
+    {
+        public List<Datum> data { get; set; }
+        public Paging paging { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Posts posts { get; set; }
+        public Comments comments { get; set; }
+        public string id { get; set; }
+    }
+    public class Cursors
+    {
+        public string before { get; set; }
+        public string after { get; set; }
+    }
+
+    public class Paging
+    {
+        public Cursors cursors { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Posts
+    {
+        public List<Datum> data { get; set; }
+        public Paging paging { get; set; }
+    }
+}

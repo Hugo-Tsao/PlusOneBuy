a20d1df948675dc9cd7b1ec0e3241d4aa7668933
Chunhui-Tsai <chunhuitsai@nine-yi.com>
2019-07-04 11:00:48 +0800

    Insert setting / list controllers

---
+++ PlusOneBuy/FBPlusOneBuy/Controllers/listController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FBPlusOneBuy.Repositories;
+
+namespace FBPlusOneBuy.Controllers
+{
+    public class listController : Controller
+    {
+        // GET: list
+        public ActionResult Index()
+        {
+            ProductRepositories productRepositories = new ProductRepositories();
+            var product=productRepositories.GetAll();
+            return View();
+        }
+    }
+}
---
+++ PlusOneBuy/FBPlusOneBuy/Repositories/ProductRepositories.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FBPlusOneBuy.Models;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace FBPlusOneBuy.Repositories
+{
+    public class ProductRepositories
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["ContextModel"].ConnectionString;
+        public IEnumerable<Product> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection.Query<Product>("SELECT * FROM Products");
+        }
+    }
+}

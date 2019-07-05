907e40531481a4c03c4d18c9ad3990224296559b
JimChou1101 <jimchou@nine-yi.com>
2019-07-06 01:27:29 +0800

    判斷關鍵字(先用+1)

---
+++ PlusOneBuy/FBPlusOneBuy/Services/CommentFilterService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FBPlusOneBuy.Repositories;
+
+namespace FBPlusOneBuy.Services
+{
+    public class CommentFilterService
+    {
+        public List<Datum> KeywordFilter(List<Datum> datas)
+        {
+            List<Datum> result = new List<Datum>();
+            foreach (var data in datas)
+            {
+                if (data.message.Equals("+1"))
+                {
+                    result.Add(data);
+                }
+            }
+            return result;
+        }
+    }
+}

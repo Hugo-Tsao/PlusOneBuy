0cd3dfdb19f0c4282ad26bcedebd52f34a88b23a
Chunhui-Tsai <chunhuitsai@nine-yi.com>
2019-07-06 18:35:10 +0800

    Insert Product

--- PlusOneBuy/FBPlusOneBuy/Controllers/listController.cs
+++ PlusOneBuy/FBPlusOneBuy/Controllers/listController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using FBPlusOneBuy.Repositories;
 using Newtonsoft.Json;
+using FBPlusOneBuy.Models;
 
 namespace FBPlusOneBuy.Controllers
 {
@@ -23,7 +24,10 @@ namespace FBPlusOneBuy.Controllers
             ViewData["keyWord"] = keyWord;
             ViewData["ProductName"] = ProductName;
             ViewData["liveID"] = liveID;
-
+            ProductRepositories productRepositories = new ProductRepositories();
+            var product = new List<Product>();
+            product=productRepositories.FindByName(ProductName).ToList();
+            ViewData["product"] = product;
             return View();
         }
         [HttpPost]

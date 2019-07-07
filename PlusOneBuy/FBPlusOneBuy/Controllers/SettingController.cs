14cb4d39dbe49d7fd4109919ef43849529eaaae3
Chunhui-Tsai <chunhuitsai@nine-yi.com>
2019-07-07 17:58:40 +0800

    Insert Video

--- PlusOneBuy/FBPlusOneBuy/Controllers/SettingController.cs
+++ PlusOneBuy/FBPlusOneBuy/Controllers/SettingController.cs
@@ -17,12 +17,26 @@ namespace FBPlusOneBuy.Controllers
         // GET: Setting
         public ActionResult Index()
         {
-            string fanPageName=Session["fanPageName"].ToString();
-            string token= Session["token"].ToString();
+            return View();
+        }
+        public ActionResult FanPageName()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public ActionResult GetFanPageData(string token,string fanPageName)
+        {
+            Session["fanPageName"] = fanPageName;
+            //Session["token"] = token;
+            Session["token"] = "EAASxbKYYpHoBAI27CZBoK8ZBzFmJjEMIR30woKcIfDPx4mtljSUOsGxVGsKHmy1JgCay8KTilT9l3nbkSfGzBZC6wVSDUcl3ZAa7C5OyZAv8CV7K0duuyW2jHFGqZCwhIKiM6jPonrHLp7s5UEudWL5UHkT8IuZBGmBTOEHS0IjYZCsYbcQfo3j9";
+            return Json(Url.Action("video", "Setting"));
 
-            //string fanPageName = "justTshirt.tw";
-            //string token = "EAASxbKYYpHoBAI27CZBoK8ZBzFmJjEMIR30woKcIfDPx4mtljSUOsGxVGsKHmy1JgCay8KTilT9l3nbkSfGzBZC6wVSDUcl3ZAa7C5OyZAv8CV7K0duuyW2jHFGqZCwhIKiM6jPonrHLp7s5UEudWL5UHkT8IuZBGmBTOEHS0IjYZCsYbcQfo3j9";
-            var result=FBRequestService.getLiveID(fanPageName, token);
+        }
+        public ActionResult Video()
+        {
+            string fanPageName = Session["fanPageName"].ToString();
+            string token = Session["token"].ToString();
+            var result = FBRequestService.getLiveID(fanPageName, token);
             ViewData["liveIDList"] = result;
 
             string ProductName = "馬桶泡泡洗";
@@ -32,18 +46,6 @@ namespace FBPlusOneBuy.Controllers
             ViewData["product"] = product;
 
             return View();
-        }
-        public ActionResult FanPageName()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult GetFanPageData(string token,string fanPageName)
-        {
-            Session["fanPageName"] = fanPageName;
-            Session["token"] = token;
-            return Json(Url.Action("Index", "Setting"));
-
         }
     }
 }

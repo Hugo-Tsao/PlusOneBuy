81f82fda81b5536a00040cf7eb2b79f7e7833bf8
Hugo-Tsao <hugotsao@nine-yi.com>
2019-07-08 14:06:56 +0800

    測試是否取得key

---
+++ PlusOneBuy/FBPlusOneBuy/Controllers/textCatchKeyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FBPlusOneBuy.Controllers
+{
+    public class textCatchKeyController : Controller
+    {
+        // GET: textCatchKey
+        public ActionResult Index()
+        {
+            var value = ConfigurationManager.AppSettings["X-API-KEY"];
+            ViewData["key"] = value;
+            return View();
+        }
+    }
+}

348b1a61955e542d5cecec28d5f46beda012ff9d
Hugo-Tsao <hugotsao@nine-yi.com>
2019-07-08 18:42:51 +0800

    新增讀取商品api

--- PlusOneBuy/FBPlusOneBuy/Controllers/ProductController.cs
+++ PlusOneBuy/FBPlusOneBuy/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Results;
 using System.Web.Mvc;
 using FBPlusOneBuy.Models;
 using FBPlusOneBuy.Services;
@@ -13,23 +14,40 @@ namespace FBPlusOneBuy.Controllers
         [HttpPost]
         public ActionResult GetSKUListByMain(int salepage_id)
         {
-            
+            List<Store_Data> data = new List<Store_Data>();
             Store store = ProductService.GetSKUListByMain(salepage_id);
-            return Json(store);
+            data = store.Data;
+            if (data.Count != 0)
+            {
+                return Json(store);
+            }
+            else
+            {
+                return Json("");
+            }
         }
-
-        [HttpPost]
-        public ActionResult GetStock(int salepage_id, int SkuId)
-        {
-            Store store = ProductService.GetStock(salepage_id, SkuId);
-            return Json(store);
-        }
-
         [HttpPost]
         public ActionResult GetMain(int salepage_id)
         {
+            List<Store_Data> data = new List<Store_Data>();
             Store store = ProductService.GetMain(salepage_id);
-            return Json(store);
+            data = store.Data;
+            if (data.Count != 0)
+            {
+                return Json(store);
+            }
+            else
+            {
+                return Json("");
+            }
         }
+        //[HttpPost]
+        //public ActionResult GetStock(int salepage_id, int SkuId)
+        //{
+        //    Store store = ProductService.GetStock(salepage_id, SkuId);
+        //    return Json(store);
+        //}
+
+        
     }
 }
--- PlusOneBuy/FBPlusOneBuy/Services/ProductService.cs
+++ PlusOneBuy/FBPlusOneBuy/Services/ProductService.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using FBPlusOneBuy.Models;
+using FBPlusOneBuy.Repositories;
 using RestSharp;
 
 namespace FBPlusOneBuy.Services
 {
     public static class ProductService
     {
+        //儲存所有商品
+        [WebMethod(EnableSession = true)] //啟用Session
+        public static ProductList GetCurrentCart()
+        {
+            //確認HttpContext.Current是否為空
+            if (HttpContext.Current != null)
+            {
+                //如果Session["Cart"]不存在，就建一個空的Cart物件
+                if (HttpContext.Current.Session["Cart"] == null)
+                {
+                    var order = new ProductList();
+                    HttpContext.Current.Session["Cart"] = order;
+                }
+
+                return (ProductList)HttpContext.Current.Session["Cart"];
+            }
+            else
+            {
+                throw new InvalidOperationException("System.web.HttpContext.Current為空，請檢查");
+            }
+        }
+
+        public static void ClearCart()
+        {
+            HttpContext.Current.Session["Cart"] = new ProductList();
+        }
         internal static string keyValue = ConfigurationManager.AppSettings["X-API-KEY"];
         public static Store GetSKUListByMain(int salepage_id)
         {
@@ -23,7 +51,7 @@ namespace FBPlusOneBuy.Services
             request.AddHeader("Cache-Control", "no-cache");
             request.AddHeader("x-api-key", keyValue);
             request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("undefined", "{\r\n  \"id\": "+ salepage_id + "\r\n}", ParameterType.RequestBody);
+            request.AddParameter("undefined", "{\r\n  \"id\": " + salepage_id + "\r\n}", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
 
             Store store = Newtonsoft.Json.JsonConvert.DeserializeObject<Store>(response.Content);
@@ -43,7 +71,7 @@ namespace FBPlusOneBuy.Services
             request.AddHeader("Cache-Control", "no-cache");
             request.AddHeader("x-api-key", keyValue);
             request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("undefined", "{\r\n  \"id\": "+ salepage_id +"\r\n}", ParameterType.RequestBody);
+            request.AddParameter("undefined", "{\r\n  \"id\": " + salepage_id + "\r\n}", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
 
             Store store = Newtonsoft.Json.JsonConvert.DeserializeObject<Store>(response.Content);

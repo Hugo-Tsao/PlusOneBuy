0be5758ebf1bfa9f8009d6928cf78d5d9607b1d6
Chunhui-Tsai <chunhuitsai@nine-yi.com>
2019-07-17 16:24:55 +0800

    合併

--- PlusOneBuy/FBPlusOneBuy/Services/ProductService.cs
+++ PlusOneBuy/FBPlusOneBuy/Services/ProductService.cs
@@ -70,9 +70,8 @@ namespace FBPlusOneBuy.Services
 
         }
 
-        //internal static string keyValue = ConfigurationManager.AppSettings["X-API-KEY"];
-        internal static string keyValue = "AA0MXe9ZiO2t8z8cE8HFC1VkhZ7duTY9gtWAGR18";
-        
+        internal static string keyValue = ConfigurationManager.AppSettings["X-API-KEY"];
+
 
         public static ProductSKUList GetSKUListByMain(int salepage_id)
         {

d42fce469bcd9dca26f3dc518b543166e1b77176
Hugo-Tsao <hugotsao@nine-yi.com>
2019-07-31 15:10:50 +0800

    新增活動進資料庫，並加上搜尋商品

---
+++ PlusOneBuy/FBPlusOneBuy/Repositories/CompaignRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+using FBPlusOneBuy.ViewModels;
+
+namespace FBPlusOneBuy.Repositories
+{
+    public class CompaignRepository
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["Context"].ConnectionString;
+        private SqlConnection conn;
+        public bool InsertGroupBuy(CompaignViewModel cvm)
+        {
+            try
+            {
+                using (conn = new SqlConnection(connectionString))
+                {
+                    string sql =
+                        "INSERT INTO Compaign(CompaignID,GroupID,ProductID,ProductSet,PeopleGroup,Keyword,PostTime,EndTime,Detail) VALUES(@CompaignID, @GroupID, @ProductID, @ProductSet, @PeopleGroup, @Keyword, @PostTime, @EndTime, @Detail)";
+                    conn.Execute(sql, new { cvm.CompaignID, cvm.GroupID, cvm.ProductID, cvm.ProductSet, cvm.PeopleGroup, cvm.Keyword, cvm.PostTime , cvm.EndTime, cvm.Detail });
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+            
+        }
+    }
+}

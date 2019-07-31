1392c5c548d161583e4d900d637504db1ee5580b
Chunhui-Tsai <chunhuitsai@nine-yi.com>
2019-07-31 15:49:15 +0800

    搜尋活動在過濾留言

--- PlusOneBuy/FBPlusOneBuy/Controllers/LineWebHookSampleController.cs
+++ PlusOneBuy/FBPlusOneBuy/Controllers/LineWebHookSampleController.cs
@@ -1,3 +1,4 @@
+using FBPlusOneBuy.Services;
 using isRock.LineBot;
 using System;
 using System.Collections.Generic;
@@ -17,18 +18,21 @@ namespace FBPlusOneBuy.Controllers
         List<string> AdminUser = new List<string>()
         { ConfigurationManager.AppSettings["AdminUserId"],ConfigurationManager.AppSettings["AdminUserId2"],ConfigurationManager.AppSettings["AdminUserId3"]};
 
+        
+
         [Route("api/LineWebHook")]
         [HttpPost]
         public IHttpActionResult POST()
         {
             try
             {
+                DateTime time = DateTime.Now;
+                var result = CompaignService.GetCompaign(time);
+                
                 Regex re;
                 LineUserInfo UserInfo = null;
 
-                string Keyword = "我要史努比";
-                string pattern = "^" + Keyword + "\\s?\\+\\d{1}\\s*$";
-                re = new Regex(pattern);
+                
 
                 //設定ChannelAccessToken(或抓取Web.Config)
                 this.ChannelAccessToken = channelAccessToken;
@@ -38,17 +42,34 @@ namespace FBPlusOneBuy.Controllers
                 if (LineEvent.replyToken == "00000000000000000000000000000000") return Ok();
                 //回覆訊息
                 if (LineEvent.type == "message")
-                {            
-                    if (LineEvent.message.type == "text"&&re.IsMatch(LineEvent.message.text))
-                    {
-                        foreach (var AdminUserId in AdminUser)
-                        {
-                            var qty = int.Parse(LineEvent.message.text.Substring(LineEvent.message.text.IndexOf("+", StringComparison.Ordinal)));
-                            UserInfo = isRock.LineBot.Utility.GetGroupMemberProfile(LineEvent.source.groupId, LineEvent.source.userId, ChannelAccessToken);
-                            this.PushMessage(AdminUserId, "群組編號:\n" + LineEvent.source.groupId + "\n顧客編號:\n" + LineEvent.source.userId +
-                                "\n顧客照片:\n" + UserInfo.pictureUrl + "\n名字:" + UserInfo.displayName + "\n購買:" + Keyword + "\n數量:" + qty);
+                {
+                    string groupid = LineEvent.source.groupId;
 
+                    if (LineEvent.message.type == "text")
+                    {                        
+                        for (var i = 0; i < result.Count; i++)
+                        {
+                            //string Keyword = ;
+                            string pattern = "^" + result[i].Keyword + "\\s?\\+\\d{1}\\s*$";
+                            re = new Regex(pattern);
+
+                            if (groupid.Equals(result[i].GroupID) && re.IsMatch(LineEvent.message.text))
+                            {
+                                var qty = int.Parse(LineEvent.message.text.Substring(LineEvent.message.text.IndexOf("+", StringComparison.Ordinal)));
+                                UserInfo = isRock.LineBot.Utility.GetGroupMemberProfile(LineEvent.source.groupId, LineEvent.source.userId, ChannelAccessToken);
+
+                                DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                                dateTime = dateTime.AddSeconds(LineEvent.timestamp / 1000).ToLocalTime();
+
+                                foreach (var AdminUserId in AdminUser)
+                                {
+                                    this.PushMessage(AdminUserId, "活動編號:"+result[i].CompaignID+"\n群組編號:\n" + LineEvent.source.groupId + "\n顧客編號:\n" + LineEvent.source.userId +
+                                        "\n顧客照片:\n" + UserInfo.pictureUrl + "\n名字:" + UserInfo.displayName + "\n購買:" + result[i].Keyword + "\n數量:" + qty+"\n留言時間:\n"+dateTime);
+                                }
+                            }
                         }
+                        
+                        
 
                     }
                     if (LineEvent.message.type == "sticker")
---
+++ PlusOneBuy/FBPlusOneBuy/Repositories/CompaignRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+using FBPlusOneBuy.Models;
+
+namespace FBPlusOneBuy.Repositories
+{
+    public class CompaignRepository
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["Context"].ConnectionString;
+        private SqlConnection conn;
+        public IEnumerable<Compaign> GetALL()
+        {
+            using (conn=new SqlConnection(connectionString))
+            {
+                string url = "SELECT * FROM Compaign";
+                return conn.Query<Compaign>(url);
+            }
+        }
+
+    }
+}

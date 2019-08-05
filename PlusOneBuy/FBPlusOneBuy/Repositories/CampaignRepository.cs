b20f92cc6d7c7f5931116289526d975638066bf1
JimChou1101 <jimchou@nine-yi.com>
2019-08-05 16:34:59 +0800

    select活動repo

--- PlusOneBuy/FBPlusOneBuy/Repositories/CampaignRepository.cs
+++ PlusOneBuy/FBPlusOneBuy/Repositories/CampaignRepository.cs
@@ -45,5 +45,27 @@ namespace FBPlusOneBuy.Repositories
                     });
             }
         }
+
+        public int SelectStoreManagerID(string userId)
+        {
+            using (conn=new SqlConnection(connectionString))
+            {
+                string sql = "SELECT StoreManagerID FROM StoreManager WHERE AspNetUserId=@AspNetUserId and Status='True';";
+                int storeManagerID = conn.QueryFirstOrDefault<int>(sql, new { AspNetUserId = userId });
+                return storeManagerID;
+            }
+        }
+
+        public List<LineListCampaignViewModel> SelectCampaigns(int storeManagerID)
+        {
+            using (conn = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT c.CampaignID,l.GroupName,p.ProductName,c.Keyword,c.ProductGroup,c.ProductSet,c.PostTime,c.EndTime,c.Title FROM AspNetUsers u INNER JOIN StoreManager s ON u.Id = s.AspNetUserId INNER JOIN LineGroup l ON s.StoreManagerID = l.StoreManagerID INNER JOIN Campaign c ON l.GroupID = c.GroupID INNER JOIN Products p ON c.ProductID = p.ProductID Where s.StoreManagerID=@storeManagerID;";
+                var campaigns = conn.Query<LineListCampaignViewModel>(sql, new { storeManagerID }).ToList();
+                return campaigns;
+            }
+        }
+
+
     }
 }
---
+++ PlusOneBuy/FBPlusOneBuy/ViewModels/LineListCampaignViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FBPlusOneBuy.ViewModels
+{
+    public class LineListCampaignViewModel
+    {
+        [Display(Name = "活動編號")]
+        public int CompaignID { get; set; }
+
+        [Display(Name = "群組名稱")]
+        public string GroupName { get; set; }
+
+        [Display(Name = "活動標題")]
+        public string Title { get; set; }
+
+        [Display(Name = "商品名稱")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "成組數")]
+        public int ProductSet { get; set; }
+
+        [Display(Name = "成團數")]
+        public int ProductGroup { get; set; }
+
+        [Display(Name = "關鍵字")]
+        public string Keyword { get; set; }
+        [Display(Name = "開始時間")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}")]
+        public DateTime PostTime { get; set; }
+        [Display(Name = "結束時間")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}")]
+        public DateTime EndTime { get; set; }
+    }
+}

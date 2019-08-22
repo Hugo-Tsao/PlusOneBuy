3cdf862698e9634dc66afb7c554035f1580a82cf
Chunhui-Tsai <chunhuitsai@nine-yi.com>
2019-08-22 15:53:11 +0800

    fb 留言圖表

--- PlusOneBuy/FBPlusOneBuy/Controllers/ChartController.cs
+++ PlusOneBuy/FBPlusOneBuy/Controllers/ChartController.cs
@@ -1,4 +1,6 @@
-﻿using FBPlusOneBuy.Services;
+﻿using FBPlusOneBuy.Repositories;
+using FBPlusOneBuy.Services;
+using FBPlusOneBuy.ViewModels;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -52,6 +54,37 @@ namespace FBPlusOneBuy.Controllers
                 price.Add(amount);
             }
             return Json(arrayData);
+        }
+        [HttpPost]
+        public ActionResult FbOrderChart(int liveId)
+        {
+            LivePostsRepository livePost_repo = new LivePostsRepository();
+            var time = livePost_repo.GetLivePost(liveId);
+
+            OrderRepositories order_repo = new OrderRepositories();
+            var orders = order_repo.GetOrders(liveId);
+
+            var post = time.postTime.Minute;
+            TimeSpan interval =  time.endTime- time.postTime ;
+            var minutes = Math.Round(Convert.ToDouble(interval.TotalMinutes));
+            var intervalOrders = new List<CommentOrderLIstViewModel>();
+                        
+            var sum = new List<int>();
+            var datetime = new List<string>();
+            var chartData = new { datetime, sum };
+            for (var i = 1; i <= minutes; i++)
+            {
+                intervalOrders = orders.Where((x)=>x.OrderDateTime.Minute>= post + i && x.OrderDateTime.Minute< post + i+1).ToList();
+                var qty = 0;
+                foreach (var item in intervalOrders)
+                {
+                     qty += item.Quantity;
+                }
+                datetime.Add(time.postTime.AddMinutes(i).ToString("HH:mm"));
+                sum.Add(qty);
+            }
+            
+            return Json(chartData);
         }
     }
 }

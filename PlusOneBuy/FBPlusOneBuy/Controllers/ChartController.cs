dea451e51b556efd689ed8ec6a4c21c1315ff0e2
Chunhui-Tsai <chunhuitsai@nine-yi.com>
2019-08-22 17:32:31 +0800

    修改合併後標記

--- PlusOneBuy/FBPlusOneBuy/Controllers/ChartController.cs
+++ PlusOneBuy/FBPlusOneBuy/Controllers/ChartController.cs
@@ -65,26 +65,27 @@ namespace FBPlusOneBuy.Controllers
             var orders = order_repo.GetOrders(liveId);
 
             var post = time.postTime.Minute;
-            TimeSpan interval =  time.endTime- time.postTime ;
+            TimeSpan interval = time.endTime - time.postTime;
             var minutes = Math.Round(Convert.ToDouble(interval.TotalMinutes));
             var intervalOrders = new List<CommentOrderLIstViewModel>();
-                        
+
             var sum = new List<int>();
             var datetime = new List<string>();
             var chartData = new { datetime, sum };
             for (var i = 1; i <= minutes; i++)
             {
-                intervalOrders = orders.Where((x)=>x.OrderDateTime.Minute>= post + i && x.OrderDateTime.Minute< post + i+1).ToList();
+                intervalOrders = orders.Where((x) => x.OrderDateTime.Minute >= post + i && x.OrderDateTime.Minute < post + i + 1).ToList();
                 var qty = 0;
                 foreach (var item in intervalOrders)
                 {
-                     qty += item.Quantity;
+                    qty += item.Quantity;
                 }
                 datetime.Add(time.postTime.AddMinutes(i).ToString("HH:mm"));
                 sum.Add(qty);
             }
-            
+
             return Json(chartData);
+        }
 
         [HttpPost]
         public ActionResult ReCordViewers(string livePageID, string numberOfViewers)
